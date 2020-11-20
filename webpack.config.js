9ec705c845dca5919af0959111f4d90fbc015e62
Andrew <alvarez.drew@gmail.com>
2020-11-20 14:15:43 -0600

    got webpack/manifest stuff working, will now cache properly and read while offline

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget App",
+      short_name: " Budget App",
+      description: "Track your budget off/online",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons")
+        }
+      ]
+    })
+  ]
+};
+
+module.exports = config;
